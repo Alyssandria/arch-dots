aac2cc0deb611b0e74bcdc949adeecb141b257f2
Alyssandria <sanchez.roniel.work@gmail.com>
2025-08-18 08:55:23 +0800

    feat: nvim find files fix

--- nvim/.config/nvim/lua/alyssandria/core/plugins/telescope.lua
+++ nvim/.config/nvim/lua/alyssandria/core/plugins/telescope.lua
@@ -72,9 +72,9 @@ return {
     vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
     vim.keymap.set("n", "<leader>ff", function()
       require("telescope.builtin").find_files({
-        hidden = true,
+        find_command = { "rg", "--files", "--hidden", "--no-ignore", "-g", "!.git" },
       })
-    end, { desc = "[F]u[Z]zy Find (include hidden & ignored)" })
+    end, { desc = "[F]ind [F]iles (include hidden & ignored)" })
     vim.keymap.set("n", "<leader>ss", builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
     vim.keymap.set("n", "<leader>sw", builtin.grep_string, { desc = "[S]earch current [W]ord" })
     vim.keymap.set("n", "<leader>fz", builtin.live_grep, { desc = "[F]u[Z]zy Find" })
